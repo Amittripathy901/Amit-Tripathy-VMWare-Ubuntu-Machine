e71ad6d44b6f11d248b85dc6838650580db689ca
Amit Tripathy <amittripathy.work@gmail.com>
2025-02-22 10:56:02 +0530

    Terrform folder added and Untracked files added

---
+++ Terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "My-Server" {
+  ami = "ami-0dee22c13ea7a9a67"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Sample Server"
+  }
+
+}
